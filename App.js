97b3556f06ddfd73aae3c7a49a7629e42a5ff480
owaisj <jamil.owais@gmail.com>
2019-08-24 21:40:31 -0500

    Layout wireframe with nativebase components

--- App.js
+++ App.js
@@ -1,25 +1,87 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StatusBar, Text, View } from 'react-native';
 import {
-  createMaterialTopTabNavigator,
-  createAppContainer
-} from 'react-navigation';
-import Home from './screens/Home';
-import List from './screens/List';
+  Container,
+  Header,
+  Title,
+  Subtitle,
+  Body,
+  Left,
+  Right,
+  Content,
+  Footer
+} from 'native-base';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Library Hours</Text>
-//     </View>
-//   );
-// }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-const MainNavigator = createMaterialTopTabNavigator({
-  Home: { screen: Home },
-  List: { screen: List }
-});
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
 
-const App = createAppContainer(MainNavigator);
+    this.setState({
+      loading: false
+    });
+  }
+  render() {
+    if (this.state.loading) {
+      return <AppLoading />;
+    }
 
-export default App;
+    return (
+      <Container style={{ paddingTop: StatusBar.currentHeight }}>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Library Hours</Title>
+            <Subtitle>Austin Public Library</Subtitle>
+          </Body>
+          <Right />
+        </Header>
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginVertical: 15
+          }}
+        >
+          <Text>An app to view the nearest open library</Text>
+          <View style={{ alignItems: 'center' }}>
+            <Text>Example Library Content Card</Text>
+            <Text>Name</Text>
+            <Text>Address</Text>
+            <Text>Hours</Text>
+          </View>
+          <Text>Multiple cards will be displayed here</Text>
+        </Content>
+        <Footer style={{ alignItems: 'center' }}>
+          <Text style={{ color: 'white' }}>Made by Owais Jamil 2019</Text>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+// React Navigation
+
+// import {
+//   createMaterialTopTabNavigator,
+//   createAppContainer
+// } from 'react-navigation';
+// import Home from './screens/Home';
+// import List from './screens/List';
+
+// const MainNavigator = createMaterialTopTabNavigator({
+//   Home: { screen: Home },
+//   List: { screen: List }
+// });
+// const App = createAppContainer(MainNavigator);
+// export default App;
---
+++ README.md
@@ -0,0 +1,7 @@
+# Austin Texas Library Hour App
+
+Find out which library near you is open at any time!
+
+## Technologies
+
+Made with React Native
